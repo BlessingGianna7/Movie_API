2dd830ab568c11649f0a70838757239276766e50
BlessingGianna7 <blessinggianna7@gmail.com>
2024-04-24 10:53:49 +0200

    First commit

---
+++ src/main/java/com/example/fullapp/Movie.java
@@ -0,0 +1,40 @@
+package com.example.fullapp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    private ObjectId id;
+
+    @Id
+    private String imdbId;
+
+    private String title;
+
+    private String  releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List <String> backdrops;
+
+
+    @DocumentReference
+    private List <Review> reviewId;
+
+
+}
